3028c2db555a6085a7c27aa13ae97409e61a8944
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-11-13 01:31:02 -0800

    rename project

--- iGottaDjangoApp/apps.py
+++ iGottaDjangoApp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class IgottaappConfig(AppConfig):
-    name = 'iGottaApp'
+    name = 'iGottaDjangoApp'
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iGotta.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iGottaDjango.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
